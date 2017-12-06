d09a232669c7b3dde2512096fb403b8efce8d93a
Cedricium <amaya.cedric@gmail.com>
2017-12-05 21:34:05 -0800

    1st FT specced out in comments, now uses unittest
    FT - functional test (follows the user story)

--- superlists/functional_tests.py
+++ superlists/functional_tests.py
@@ -1,6 +1,44 @@
 from selenium import webdriver
+import unittest
 
-browser = webdriver.Firefox()
-browser.get('http://localhost:8000')
 
-assert 'Django' in browser.title
+class NewVisitorTets(unittest.TestCase):
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+        self.browser.implicitly_wait(3)
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_can_start_a_list_and_retrieve_it_later(self):
+        # Monica has heard about a cool new online to-do app. She goes to check
+        # out it's homepage
+        self.browser.get('http://localhost:8000')
+
+        # She notices the page title and header mention to-do lists
+        self.assertIn('To-Do', self.browser.title)
+        self.fail('Finish the test!')
+
+        # She is invited to enter a to-do item straight away
+
+        # She types "Buy boyfriend a new MacBook Pro
+
+        # When she hits [Enter], the page updates, and now the page lists
+        # "1. Buy boyfriend a new MacBook Pro" as an item in a to-do list
+
+        # There is still a text box inviting her to add another item. She enters
+        # "Book vacation to Hawaii"
+
+        # The page updates again, and now shows both items on her list
+
+        # Monica wonders whether the site will remember her list. Then she sees that
+        # the site has generated a unique URL for her -- there is some explanatory text
+        # to that effect.
+
+        # She visits that URL - her to-do list is still there.
+
+        # Satisfied, Monica goes back to sleep
+
+
+if __name__ == '__main__':
+    unittest.main()
